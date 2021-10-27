14cb2a18e4f5f03ea56038aa9f62b935360059d9
Sandun <SandunWebDev@gmail.com>
2021-10-27 17:27:21 +0530

    FRONTEND: Prettified the files.

--- apps/frontend-main/src/reportWebVitals.js
+++ apps/frontend-main/src/reportWebVitals.js
@@ -1,13 +1,18 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+/* eslint unicorn/no-abusive-eslint-disable: 0 */
+/* eslint-disable */
+
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        import('web-vitals').then(
+            ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+                getCLS(onPerfEntry);
+                getFID(onPerfEntry);
+                getFCP(onPerfEntry);
+                getLCP(onPerfEntry);
+                getTTFB(onPerfEntry);
+            },
+        );
+    }
 };
 
 export default reportWebVitals;
